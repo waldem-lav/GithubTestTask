041d09d8c989dd4cea52b46b79a02de3ef78247f
waldem-lav <waldem.lav@gmail.com>
2024-10-19 20:56:01 +0300

    improve repository item view

--- app/src/main/java/com/waldemlav/githubtesttask/ui/search/SearchScreen.kt
+++ app/src/main/java/com/waldemlav/githubtesttask/ui/search/SearchScreen.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -29,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -103,8 +107,41 @@ fun RepositoryItem(
             }
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = item.name, style = MaterialTheme.typography.headlineMedium)
-            Spacer(modifier = Modifier.height(8.dp))
+            Row {
+                Column(Modifier.weight(1f)) {
+                    Text(text = item.name, style = MaterialTheme.typography.titleLarge)
+                    item.language?.let { language ->
+                        Spacer(Modifier.height(16.dp))
+                        Text(text = language)
+                    }
+                }
+                Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(Icons.Outlined.Star, stringResource(R.string.stargazers_count))
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = item.starCount.toString(), style = MaterialTheme.typography.titleLarge)
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(painterResource(R.drawable.icon_eye_24), stringResource(R.string.watchers_count))
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = item.watchersCount.toString(), style = MaterialTheme.typography.titleLarge)
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painterResource(R.drawable.icon_fork_20),
+                        stringResource(R.string.forks_count),
+                        modifier = Modifier.padding(top = 2.dp)
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = item.forksCount.toString(), style = MaterialTheme.typography.titleLarge)
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            item.description?.let { description ->
+                Text(text = description, style = MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
             Button(onClick = { downloadZip.invoke(item) }) {
                 Text(text = stringResource(id = R.string.download_zip))
             }
@@ -117,7 +154,6 @@ fun RepositoryItem(
 fun SearchScreenPreview() {
     SearchScreen(
         state = SearchScreenState(
-            errorMessage = "Unknown Error",
             userRepos = listOf(
                 GithubRepository(
                     1,
